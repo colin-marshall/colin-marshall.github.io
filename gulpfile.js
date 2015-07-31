9edad2ff3c9dcbe97143187aa81c5ac15fd89ea5
colin-marshall <colin.michael.marshall@gmail.com>
2015-07-30 21:01:14 -0600

    added bootstrap, switched sass building from jekyll to gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,38 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var cp          = require('child_process');
+var bower       = require('gulp-bower');
+var sass        = require('gulp-sass');
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
+var config = {
+     sassPaths: [
+        './bower_components/bootstrap-sass/assets/stylesheets',
+        '_sass'
+    ],
+     bowerDir: './bower_components'
+}
+
+/**
+ * Build the Sass
+ */
+
+gulp.task('compileSass', function(){
+    return gulp.src('css/main.scss')
+        .pipe(sass({
+            includePaths: config.sassPaths
+        })
+            .on('error', sass.logError))
+        .pipe(gulp.dest('_site/css'));
+});
+
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
+gulp.task('jekyll-build', ['compileSass'], function (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
         .on('close', done);
@@ -31,6 +54,15 @@ gulp.task('browser-sync', ['jekyll-build'], function() {
             baseDir: '_site'
         }
     });
+});
+
+
+/**
+ * Download Bower dependencies 
+ */
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
 });
 
 /**
